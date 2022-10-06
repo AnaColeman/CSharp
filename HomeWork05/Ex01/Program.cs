752b231e6c98ac7e8f1b052967caaea1ae3fdf6e
Ana <anastasia50192iv@gmail.com>
2022-10-06 15:44:34 +0300

    Added HW05

---
+++ HomeWork05/Ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+Console.WriteLine("Enter the number of array elements:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+RandonNumbers(numbers);
+Console.WriteLine("In the array: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+Console.WriteLine($"Of {numbers.Length} array numbers, {count} -> even");
+
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ HomeWork05/Ex03/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+double[] arrayInputDouble()
+{
+    Console.WriteLine("Enter the number of array elements:  ");
+    int count = Convert.ToInt32(Console.ReadLine());
+    double[] array = new double[count];
+	for(int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(-100, 101); 
+	}
+    return array;
+}
+
+void arrayOutput(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double diffMinMaxCount(double[] array) 
+{
+    double minElem = array[0];
+    double maxElem = array[0];
+    for(int i = 1; i < array.Length; i++)
+	{
+		if(array[i] < minElem)
+        {
+            minElem = array[i];
+        }
+        else if(array[i] > maxElem)
+        {
+            maxElem = array[i];
+        }
+	}
+    Console.WriteLine($"Max element -> {maxElem} ");
+    Console.WriteLine($"Min element -> {minElem} ");
+    double diff = maxElem - minElem;
+    return diff;
+}
+
+double[] array = arrayInputDouble();
+arrayOutput(array); 
+Console.WriteLine($"The difference between max and min array element: {diffMinMaxCount(array)} ");
