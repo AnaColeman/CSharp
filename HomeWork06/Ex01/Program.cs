83cb4a770146dabec92be881fd3856efe32d92ea
Ana <anastasia50192iv@gmail.com>
2022-10-10 17:14:54 +0300

    Added HW06

---
+++ HomeWork06/Ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Enter M numbers from the keyboard: "); 
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("User entered from keyboard: "); 
+int []array = new int[M];
+FillArray(array);
+FindPositiveNumber(array);
+
+void FillArray (int[]array)
+{
+    for (int i=0; i < M; i++)
+    {
+        array[i]=new Random().Next(-10,11);
+        Console.Write(array[i]+ ", ");
+    }
+Console.WriteLine();
+}
+
+void FindPositiveNumber(int[]array)
+{
+    int count = 0;
+    for (int j = 0; j < M; j++)
+    {
+        if (array [j] >= 0)
+        count++; 
+    }
+    Console.WriteLine($"{count} numbers -> positive (that is > 0) ");
+}
+Console.WriteLine();
---
+++ HomeWork06/Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+Console.Clear();
+void IntersectionPoint(double k1, double k2, double b1, double b2)
+{   
+    double x;
+    double y;
+    x = (b2 - b1) / (k1 - k2);
+    y = k2 * x + b2;
+    Console.Write($"An intersection point is ({x};{y})");
+    Console.WriteLine();
+}
+
+Console.Write($"Enter the first value b1: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Enter the first value k1: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Enter the first value b2: ");
+double d = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Enter the first value k2: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+IntersectionPoint(a, b, c, d);
